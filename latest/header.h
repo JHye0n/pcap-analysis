bd92279d6e9b355cfd369f21b11e078fabb823b8
jhyeon <stjhyeon@kakao.com>
2020-09-10 14:59:20 +0900

    change file(header.h)

--- latest/header.h
+++ latest/header.h
@@ -5,8 +5,8 @@
 #include <netinet/udp.h>
 
 #pragma once
-
-struct ethernet_hdr *eth_hdr
+struct ethernet_hdr *eth_hdr;
+struct ethernet_hdr
 {
     u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
     u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
