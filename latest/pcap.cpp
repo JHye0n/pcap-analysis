f824eb6375a3717aef57b53ee525f265e8b09573
jhyeon <stjhyeon@kakao.com>
2020-09-10 17:04:39 +0900

    pcap.cpp updated

--- latest/pcap.cpp
+++ latest/pcap.cpp
@@ -3,14 +3,34 @@
 #include <cstdio>
 #include <pcap.h>
 #include <iostream>
+#include <unordered_map>
 #include "header.h" // header define
+
+struct flowpacket{
+	std::string src_addr;
+	std::string dst_addr;
+	int src_port;
+	int dst_port;
+};
+
+std::unordered_map<std::string, int> flowtable;
 
 void usage(char* argv[]){
 	printf("Usage %s <pcap-file>\n", argv[0]);
 }
 
-int main(int argc, char* argv[]){
+int flow_p(struct pcap_pkthdr* header, const u_char *packet)
+{
+	eth_hdr = (struct ethernet_hdr *) packet;
 
+	if(ntohs(eth_hdr->ether_type) == ETHERTYPE_IP){
+		iphdr = (struct ip *) (packet + sizeof(ethernet_hdr));
+
+		printf("%s\n", inet_ntoa(iphdr->ip_src));
+	}
+}
+
+int main(int argc, char* argv[]){
 	// argc < 2
 	if(argc < 2){
 		usage(argv);
@@ -37,12 +57,7 @@ int main(int argc, char* argv[]){
 			break;
 		}
 
-		/*eth_hdr = (struct ethernet_hdr *) packet;
-
-		if(ntohs(eth_hdr->ether_type) == ETHERTYPE_IP){
-			iphdr = (struct ip *) (packet + sizeof(ethernet_hdr));
-
-		}*/
+		flow_p(header, packet);
 
 	}
 
