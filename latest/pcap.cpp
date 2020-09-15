f7d01ac13a8372751eae36c4660de68217e57d25
jhyeon <stjhyeon@kakao.com>
2020-09-15 20:23:19 +0900

    binary delete

--- latest/pcap.cpp
+++ latest/pcap.cpp
@@ -5,14 +5,26 @@
 #include <iostream>
 #include <map>
 #include "header.h" // header define
-#include "flow.h" // struct, class define
 #include "struct.h"
 #include "ip.h"
 
 using namespace std;
 
+map<flowpacket, int> packetinfo;
+
 void usage(char* argv[]){
 	printf("Usage %s <pcap-file>\n", argv[0]);
+}
+
+void print_packet()
+{
+	map<flowpacket, int>::iterator it;
+
+	for(it == packetinfo.begin(); it != packetinfo.end(); it++){
+
+		cout << it->first.protocol.c_str() << endl;
+	}
+
 }
 
 void pk_receive(struct pcap_pkthdr* header, const u_char *packet){
@@ -20,6 +32,8 @@ void pk_receive(struct pcap_pkthdr* header, const u_char *packet){
 	flowpacket temp;
 
 	eth_hdr = (struct ethernet_hdr *) packet;
+
+	int packetsize = packetinfo.size() + 1;
 
 	if(ntohs(eth_hdr->ether_type) == ETHERTYPE_IP){
 		
@@ -33,6 +47,11 @@ void pk_receive(struct pcap_pkthdr* header, const u_char *packet){
 			temp.src_port = ntohs(tcp_hdr->th_sport);
 			temp.dst_addr = Ip(htonl(iphdr->ip_dst.s_addr));
 			temp.dst_port = ntohs(tcp_hdr->th_dport);
+			temp.tcp_pk += 1;
+			temp.tcp_pkbyte = header->caplen;
+			//packetinfo.insert(make_pair(temp, packetsize));
+
+			printf("packet byte(a->b, b->a) %d\n", temp.tcp_pkbyte);
 
 		}else if(iphdr->ip_p == IPPROTO_UDP){
 			udp_hdr = (struct udphdr *) (packet + sizeof(ethernet_hdr) + sizeof(tcphdr));
@@ -42,8 +61,11 @@ void pk_receive(struct pcap_pkthdr* header, const u_char *packet){
 			temp.src_port = ntohs(udp_hdr->uh_sport);
 			temp.dst_addr = Ip(htonl(iphdr->ip_dst.s_addr));
 			temp.dst_port = ntohs(udp_hdr->uh_dport);
+			temp.udp_pk += 1;
+			temp.udp_pkbyte = header->caplen;
+			//packetinfo.insert(make_pair(temp2, packetsize));
 		}
-		
+
 	}
 }
 
@@ -78,8 +100,7 @@ int main(int argc, char* argv[]){
 
 	}
 
+	//print_packet();
+
 	pcap_close(handle);
 }
-
-
-
--- latest/struct.h
+++ latest/struct.h
@@ -12,6 +12,17 @@ struct flowpacket
 	u_short src_port;
 	Ip dst_addr;
 	u_short dst_port;
+	int tcp_pk = 0;
+	int tcp_pkbyte = 0;
+	int udp_pk = 0;
+	int udp_pkbyte = 0;
 
-	bool operator==(const flowpacket& otherflow) const;
+	bool operator<(const flowpacket& otherflow) const{
+		return ((src_addr < otherflow.src_addr)&&(src_port < otherflow.src_port)&&(dst_addr < otherflow.dst_addr)&&(dst_port < otherflow.dst_port));
+	}
+
+	//unordered_map operator
+	bool operator==(const flowpacket& otherflow) const{
+		return ((src_addr == otherflow.src_addr)&&(src_port == otherflow.src_port)&&(dst_addr == otherflow.dst_addr)&&(dst_port == otherflow.dst_port));
+	}
 };
